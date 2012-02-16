3b1eb9092934c3ef9cc071aa39cf68c38497c1c9
fukayatsu <fukayatsu@gmail.com>
2012-02-17 01:09:30 +0900

    git add -uを覚えた

--- spec/MyClass.spec.js
+++
@@ -1,7 +0,0 @@
-var MyClass = require('../lib/MyClass').MyClass;
-
-describe('MyClass', function() {
-  it('lengthプロパティで配列長を取得する事ができる', function() {
-    expect(MyClass.list.length).toEqual(3);
-  });
-});
